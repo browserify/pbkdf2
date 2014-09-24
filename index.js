16d209ecd2e997ca775f32b47b8c2318a55ecdd8
Daniel Cousens <github@dcousens.com>
2014-09-24 12:01:26 +1000

    pbkdf2: add async impl. and invalid tests

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
-var assert = require('assert')
 var crypto = require('crypto')
 
-module.exports = function pbkdf2Sync(password, salt, iterations, keylen, algo) {
-  if ('number' !== typeof iterations)
-    throw new TypeError('Iterations not a number')
+var exportFn = require('./pbkdf2')
+var exported = exportFn(crypto)
 
-  if (iterations < 0)
-    throw new TypeError('Bad iterations')
+module.exports = {
+  pbkdf2: exported.pbkdf2,
+  pbkdf2Sync: exported.pbkdf2Sync,
 
-  if ('number' !== typeof keylen)
-    throw new TypeError('Key length not a number')
-
-  if (keylen < 0)
-    throw new TypeError('Bad key length')
-
-  algo = algo || 'sha1'
-
-  if (!Buffer.isBuffer(password)) password = new Buffer(password)
-  if (!Buffer.isBuffer(salt)) salt = new Buffer(salt)
-
-  var hLen, l = 1, r, T
-  var DK = new Buffer(keylen)
-  var block1 = new Buffer(salt.length + 4)
-  salt.copy(block1, 0, 0, salt.length)
-
-  for (var i = 1; i <= l; i++) {
-    block1.writeUInt32BE(i, salt.length)
-
-    var U = crypto.createHmac(algo, password).update(block1).digest()
-
-    if (!hLen) {
-      hLen = U.length
-      T = new Buffer(hLen)
-      l = Math.ceil(keylen / hLen)
-      r = keylen - (l - 1) * hLen
-
-      assert(keylen <= (Math.pow(2, 32) - 1) * hLen, 'keylen exceeds maximum length')
-    }
-
-    U.copy(T, 0, 0, hLen)
-
-    for (var j = 1; j < iterations; j++) {
-      U = crypto.createHmac(algo, password).update(U).digest()
-
-      for (var k = 0; k < hLen; k++) {
-        T[k] ^= U[k]
-      }
-    }
-
-    var destPos = (i - 1) * hLen
-    var len = (i == l ? r : hLen)
-    T.copy(DK, destPos, 0, len)
-  }
-
-  return DK
+  // for crypto-browserify
+  __pbkdf2Export: exportFn
 }
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var assert = require('assert')
-var pbkdf2 = require('../')
+var compat = require('../')
 
 var fixtures = require('./fixtures')
 
@@ -8,33 +8,68 @@ var fixtures = require('./fixtures')
 // https://stackoverflow.com/questions/5130513/pbkdf2-hmac-sha2-test-vectors
 // https://stackoverflow.com/questions/15593184/pbkdf2-hmac-sha-512-test-vectors
 
-describe('pbkdf2', function() {
-  it('defaults to sha1', function() {
-    var result = pbkdf2('password', 'salt', 1, 32)
+describe('pbkdf2-compat', function() {
+  describe('pbkdf2', function() {
+    ;['sha1', 'sha256', 'sha512'].forEach(function(algorithm) {
+      describe(algorithm, function() {
+        fixtures.valid.forEach(function(f, i) {
+          var expected = f.results[algorithm]
 
-    assert.equal(result.toString('hex'), "0c60c80f961f0e71f3a9b524af6012062fe037a6e0f0eb94fe8fc46bdc637164")
-  })
+          it('Async test case ' + i + ' for ' + algorithm + ' matches ' + expected, function(done) {
+            compat.pbkdf2(f.key, f.salt, f.iterations, f.dkLen, algorithm, function(err, result) {
+              assert.equal(result.toString('hex'), expected)
 
-  ;['sha1', 'sha256', 'sha512'].forEach(function(algorithm) {
-    describe(algorithm, function() {
-      fixtures.valid.forEach(function(f, i) {
-        var expected = f.results[algorithm]
+              done()
+            })
+          })
+        })
 
-        it('Test case ' + i + ' for ' + algorithm + ' matches ' + expected, function() {
-          var result = pbkdf2(f.key, f.salt, f.iterations, f.dkLen, algorithm)
+        fixtures.invalid.forEach(function(f) {
+          it('should throw ' + f.exception, function(done) {
+            compat.pbkdf2(f.key, f.salt, f.iterations, f.dkLen, f.algo, function(err) {
+              assert(new RegExp(f.exception).test(err))
 
-          assert.equal(result.toString('hex'), expected)
+              done()
+            })
+          })
         })
       })
+    })
 
-      fixtures.invalid.forEach(function(f) {
-        it('should throw ' + f.description, function() {
-          assert.throws(function() {
-            pbkdf2(f.algo, f.key, f.salt, f.iterations, f.dkLen)
-          }, new RegExp(f.exception))
+    it('should throw if no callback is provided', function() {
+      assert.throws(function() {
+        compat.pbkdf2('password', 'salt', 1, 32, 'sha1')
+      }, /No callback provided to pbkdf2/)
+    })
+  })
+
+  describe('pbkdf2Sync', function() {
+    it('defaults to sha1', function() {
+      var result = compat.pbkdf2Sync('password', 'salt', 1, 32)
+
+      assert.equal(result.toString('hex'), "0c60c80f961f0e71f3a9b524af6012062fe037a6e0f0eb94fe8fc46bdc637164")
+    })
+
+    ;['sha1', 'sha256', 'sha512'].forEach(function(algorithm) {
+      describe(algorithm, function() {
+        fixtures.valid.forEach(function(f, i) {
+          var expected = f.results[algorithm]
+
+          it('Test case ' + i + ' for ' + algorithm + ' matches ' + expected, function() {
+            var result = compat.pbkdf2Sync(f.key, f.salt, f.iterations, f.dkLen, algorithm)
+
+            assert.equal(result.toString('hex'), expected)
+          })
+        })
+
+        fixtures.invalid.forEach(function(f) {
+          it('should throw ' + f.exception, function() {
+            assert.throws(function() {
+              compat.pbkdf2Sync(f.key, f.salt, f.iterations, f.dkLen, f.algo)
+            }, new RegExp(f.exception))
+          })
         })
       })
     })
   })
 })
-
