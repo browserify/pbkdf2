e51121f46887e545fedd817ac8624e0d68c9ace5
Daniel Cousens <github@dcousens.com>
2014-10-21 00:24:00 +1100

    index: remove unnecessary __pbkdf2Export

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ var exported = exportFn(crypto)
 
 module.exports = {
   pbkdf2: exported.pbkdf2,
-  pbkdf2Sync: exported.pbkdf2Sync,
-
-  // for crypto-browserify
-  __pbkdf2Export: exportFn
+  pbkdf2Sync: exported.pbkdf2Sync
 }
