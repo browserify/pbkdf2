21fee9ce4a66155986485a6bb5f9ddbf220b2c45
Daniel Cousens <github@dcousens.com>
2016-01-13 11:29:04 +1100

    browser: remove r calculation

--- browser.js
+++ browser.js
@@ -39,7 +39,6 @@ exports.pbkdf2Sync = function (password, salt, iterations, keylen, digest) {
   var block1 = new Buffer(salt.length + 4)
   salt.copy(block1, 0, 0, salt.length)
 
-  var r
   var T
 
   for (var i = 1; i <= l; i++) {
@@ -50,7 +49,6 @@ exports.pbkdf2Sync = function (password, salt, iterations, keylen, digest) {
       hLen = U.length
       T = new Buffer(hLen)
       l = Math.ceil(keylen / hLen)
-      r = keylen - (l - 1) * hLen
     }
 
     U.copy(T, 0, 0, hLen)
@@ -60,8 +58,7 @@ exports.pbkdf2Sync = function (password, salt, iterations, keylen, digest) {
       for (var k = 0; k < hLen; k++) T[k] ^= U[k]
     }
 
-    var len = (i === l ? r : hLen)
-    T.copy(DK, destPos, 0, len)
+    T.copy(DK, destPos, 0, hLen)
     destPos += hLen
   }
 
