9a76e2f37ef4541aeff92d97b7335b7fa6e989a8
Jordan Harband <ljharb@gmail.com>
2025-06-02 15:52:11 -0700

    [lint] fix whitespace

--- bench/index.js
+++ bench/index.js
@@ -6,30 +6,30 @@ run('hello', 'world', 100, 32, 'md5')
 run('hello', 'world', 100, 32, 'sha256')
 run('hello', 'world', 10000, 70, 'md5')
 run('hello', 'world', 10000, 70, 'sha256')
-function run (password, salt, iterations, keylen, digest) {
-  var suite = new Benchmark.Suite()
-  console.log(`password: ${password}`)
-  console.log(`salt: ${salt}`)
-  console.log(`iterations: ${iterations}`)
-  console.log(`keylen: ${keylen}`)
-  console.log(`digest: ${digest}`)
+function run(password, salt, iterations, keylen, digest) {
+	var suite = new Benchmark.Suite()
+	console.log(`password: ${password}`)
+	console.log(`salt: ${salt}`)
+	console.log(`iterations: ${iterations}`)
+	console.log(`keylen: ${keylen}`)
+	console.log(`digest: ${digest}`)
 
-  // add tests
-  suite
-    .add('current', function () {
-      current(password, salt, iterations, keylen, digest)
-    })
-    .add('old', function () {
-      old(password, salt, iterations, keylen, digest)
-    })
+	// add tests
+	suite
+		.add('current', function () {
+			current(password, salt, iterations, keylen, digest)
+		})
+		.add('old', function () {
+			old(password, salt, iterations, keylen, digest)
+		})
 
-    // add listeners
-    .on('cycle', function (event) {
-      console.log(String(event.target))
-    })
-    .on('complete', function () {
-      console.log('Fastest is ' + this.filter('fastest').map('name'))
-      console.log('')
-    })
-    .run()
+	// add listeners
+		.on('cycle', function (event) {
+			console.log(String(event.target))
+		})
+		.on('complete', function () {
+			console.log('Fastest is ' + this.filter('fastest').map('name'))
+			console.log('')
+		})
+		.run()
 }
--- index.js
+++ index.js
@@ -5,35 +5,35 @@ var defaultEncoding = require('./lib/default-encoding')
 var toBuffer = require('./lib/to-buffer')
 
 function nativePBKDF2(password, salt, iterations, keylen, digest, callback) {
-  checkParameters(iterations, keylen)
-  password = toBuffer(password, defaultEncoding, 'Password')
-  salt = toBuffer(salt, defaultEncoding, 'Salt')
+	checkParameters(iterations, keylen)
+	password = toBuffer(password, defaultEncoding, 'Password')
+	salt = toBuffer(salt, defaultEncoding, 'Salt')
 
-  if (typeof digest === 'function') {
-    callback = digest
-    digest = 'sha1'
-  }
-  if (typeof callback !== 'function') { throw new Error('No callback provided to pbkdf2') }
+	if (typeof digest === 'function') {
+		callback = digest
+		digest = 'sha1'
+	}
+	if (typeof callback !== 'function') { throw new Error('No callback provided to pbkdf2') }
 
-  return native.pbkdf2(password, salt, iterations, keylen, digest, callback)
+	return native.pbkdf2(password, salt, iterations, keylen, digest, callback)
 }
 
 function nativePBKDF2Sync(password, salt, iterations, keylen, digest) {
-  checkParameters(iterations, keylen)
-  password = toBuffer(password, defaultEncoding, 'Password')
-  salt = toBuffer(salt, defaultEncoding, 'Salt')
-  digest = digest || 'sha1'
-  return native.pbkdf2Sync(password, salt, iterations, keylen, digest)
+	checkParameters(iterations, keylen)
+	password = toBuffer(password, defaultEncoding, 'Password')
+	salt = toBuffer(salt, defaultEncoding, 'Salt')
+	digest = digest || 'sha1'
+	return native.pbkdf2Sync(password, salt, iterations, keylen, digest)
 }
 
 /* istanbul ignore next */
 if (!native.pbkdf2Sync || native.pbkdf2Sync.toString().indexOf('keylen, digest') === -1) {
-  /* eslint global-require: 0 */
-  exports.pbkdf2Sync = require('./lib/sync')
-  exports.pbkdf2 = require('./lib/async')
+	/* eslint global-require: 0 */
+	exports.pbkdf2Sync = require('./lib/sync')
+	exports.pbkdf2 = require('./lib/async')
 
 // native
 } else {
-  exports.pbkdf2Sync = nativePBKDF2Sync
-  exports.pbkdf2 = nativePBKDF2
+	exports.pbkdf2Sync = nativePBKDF2Sync
+	exports.pbkdf2 = nativePBKDF2
 }
