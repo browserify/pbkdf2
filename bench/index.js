6fd84bf64a423d207d9e665f3e9fd88b148c4480
Jordan Harband <ljharb@gmail.com>
2025-06-02 15:57:44 -0700

    [lint] fix parens/curlies/semis/etc

--- bench/index.js
+++ bench/index.js
@@ -1,35 +1,38 @@
-var Benchmark = require('benchmark')
-var current = require('../lib/sync')
-var old = require('./old')
+'use strict';
 
-run('hello', 'world', 100, 32, 'md5')
-run('hello', 'world', 100, 32, 'sha256')
-run('hello', 'world', 10000, 70, 'md5')
-run('hello', 'world', 10000, 70, 'sha256')
+var Benchmark = require('benchmark');
+var current = require('../lib/sync');
+var old = require('./old');
+
+run('hello', 'world', 100, 32, 'md5');
+run('hello', 'world', 100, 32, 'sha256');
+run('hello', 'world', 10000, 70, 'md5');
+run('hello', 'world', 10000, 70, 'sha256');
 function run(password, salt, iterations, keylen, digest) {
-	var suite = new Benchmark.Suite()
-	console.log(`password: ${password}`)
-	console.log(`salt: ${salt}`)
-	console.log(`iterations: ${iterations}`)
-	console.log(`keylen: ${keylen}`)
-	console.log(`digest: ${digest}`)
+	var suite = new Benchmark.Suite();
+	console.log(`password: ${password}`);
+	console.log(`salt: ${salt}`);
+	console.log(`iterations: ${iterations}`);
+	console.log(`keylen: ${keylen}`);
+	console.log(`digest: ${digest}`);
 
 	// add tests
 	suite
 		.add('current', function () {
-			current(password, salt, iterations, keylen, digest)
+			current(password, salt, iterations, keylen, digest);
 		})
 		.add('old', function () {
-			old(password, salt, iterations, keylen, digest)
+			old(password, salt, iterations, keylen, digest);
 		})
 
 	// add listeners
 		.on('cycle', function (event) {
-			console.log(String(event.target))
+			console.log(String(event.target));
 		})
 		.on('complete', function () {
-			console.log('Fastest is ' + this.filter('fastest').map('name'))
-			console.log('')
+			// eslint-disable-next-line no-invalid-this
+			console.log('Fastest is ' + this.filter('fastest').map('name'));
+			console.log('');
 		})
-		.run()
+		.run();
 }
